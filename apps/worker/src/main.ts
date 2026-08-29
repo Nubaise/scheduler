0fd58bf70037479b3c627979d89d9892871b4e84
Nubaise <113296164+Nubaise@users.noreply.github.com>
2026-08-29 13:29:46 +0530

    chore: scaffold NestJS worker

---
+++ apps/worker/src/main.ts
@@ -0,0 +1,9 @@
+﻿import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  await app.close();
+}
+
+await bootstrap();
